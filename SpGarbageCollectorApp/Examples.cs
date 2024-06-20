289e9803b2e7d8de9f541e239ca00adb847d73cf
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-06-20 19:47:06 +0300

    Добавьте файлы проекта.

---
+++ SpGarbageCollectorApp/Examples.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpGarbageCollectorApp
+{
+    class Employee : IDisposable
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; } = 1;
+
+        public Employee()
+        {
+            Name = "Anonim";
+            Age = 1;
+            Console.WriteLine("Employee construct");
+        }
+
+        //~Employee()
+        //{
+        //    Console.WriteLine("Employee destruct");
+        //}
+
+        public void Dispose()
+        {
+            Console.WriteLine("Employee dispose");
+        }
+    }
+    public class Examples
+    {
+        public static void GarbageCollectorExample()
+        {
+            Console.WriteLine($"TotalMemory {GC.GetTotalMemory(false)}");
+
+            Employee bob = new();
+
+            Console.WriteLine($"TotalMemory {GC.GetTotalMemory(false)}");
+
+            Employee employee;
+            for (int i = 0; i < 10000; i++)
+                employee = new();
+
+            Console.WriteLine($"TotalMemory {GC.GetTotalMemory(false)}");
+            Console.WriteLine($"Bob Generation {GC.GetGeneration(bob)}");
+
+            GC.Collect();
+            //GC.WaitForPendingFinalizers();
+
+            Console.WriteLine($"TotalMemory {GC.GetTotalMemory(false)}");
+            Console.WriteLine($"Bob Generation {GC.GetGeneration(bob)}");
+        }
+
+        public static void DisposableExample()
+        {
+            Func();
+
+            //using Employee employee = new();
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    using (employee = new Employee())
+            //    {
+
+            //    }
+
+            //}
+
+            //Thread.Sleep(2000);
+
+            
+        }
+
+        public static void Func()
+        {
+            using Employee employee = new();
+
+            Console.WriteLine(employee.Name);
+        }
+    }
+}
---
+++ SpGarbageCollectorApp/Program.cs
@@ -0,0 +1,69 @@
+﻿
+unsafe
+{
+    int n = 120;
+    int* ptr = &n;
+    Console.WriteLine($"{*ptr}");
+
+    Node node = new();
+    Node* nodePointer = &node;
+    nodePointer->Name = "Peet";
+
+
+    int[] nums = [1, 2, 3, 4, 5];
+    fixed(int* iptr = nums)
+    {
+        Console.WriteLine(*(iptr+2));
+    }
+      
+
+
+    //Method(out userPointer);
+    //GC.Collect();
+    //{
+    //    User user = new User();
+    //    userPointer = &user;
+    //}
+    //GC.Collect();
+
+    //userPointer->Name = "user";
+
+
+    //Console.WriteLine(userPointer->Name);
+
+
+
+
+
+    int* array = stackalloc int[10];
+    int[] arr = new int[10];
+
+}
+
+
+unsafe void Method(out User* userPointer)
+{
+    User user = new();
+    user.Name = "Bobby";
+    userPointer = &user;
+}
+
+unsafe struct ValueStruct
+{
+    int* ptr;
+}
+
+unsafe class PtrClass
+{
+    int* ptr;
+}
+
+class User
+{
+    public string Name { get; set; }
+}
+
+struct Node
+{
+    public string Name;
+}
